5212ddf0e8f62820b979c5931b70c5523946fb49
Vlad <vladislav.meleshko@gmail.com>
2023-01-17 15:43:05 +0300

    1

--- forms/travelings/close_traveling.cs
+++ forms/travelings/close_traveling.cs
@@ -4,11 +4,14 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using System.Text.Json;
 
 namespace Mekus.forms.travelings
 {
